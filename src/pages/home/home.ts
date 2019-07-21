478887e7050aa59eab0dc441fb5f0104f7ec9798
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2019-07-21 15:43:08 -0300

    C02 - Getting user input with alert component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public todos = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertController: AlertController) {
 
+  }
+
+  openTodoAlert() {
+    let addTodoAlert = this.alertController.create({
+      title: "Add A Todo",
+      message: "Enter your todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Todo",
+          handler: (inputData)=> {
+            let todoText: any;
+            todoText = inputData.addTodoInput;
+            this.todos.push(todoText);
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
   }
 
 }
